be87198e3d7abe44fc9cc1973c340bd15a25e70e
fella404 <tomhagen849@gmail.com>
2025-11-20 11:38:52 +0700

    chore(security): implement CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+app.use(
+  cors({
+    origin: [process.env.ADMIN_DASHBOARD_URL, process.env.FRONTEND_URL],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
